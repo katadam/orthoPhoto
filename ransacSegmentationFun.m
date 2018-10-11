13c58dc6d2020a0f8a1810db97a662c045e577fb
katadam <adam.katerin@gmail.com>
2018-10-11 14:23:06 +0300

    Add files via upload

--- ransacSegmentationFun.m
+++ ransacSegmentationFun.m
@@ -2,7 +2,7 @@ function planes = ransacSegmentationFun(XYZ1, ransacThreshold)
 
 format long 
 
-%an einai alitheia i istoria tou ..XYZ = obj.v;
+%XYZ = obj.v;
 XYZ = XYZ1(:, 1:3);
 
 i = 1;
@@ -10,7 +10,7 @@ i = 1;
 while (size(XYZ, 1) > 3)
  
     %set a RANSAC threshold appropriate for your data. In this case RANSAC threshold is set to 1.
-    [~, ~, inliers, ~] = ransacfitplane(XYZ', ransacThreshold);
+    [B, P, inliers, A] = ransacfitplane(XYZ', ransacThreshold);
  
     for j = 1 : size(inliers, 1)
      
@@ -24,7 +24,7 @@ while (size(XYZ, 1) > 3)
  
     i = i + 1;
  
-    clear inliers inliersXYZ
+    clear B P inliers A inliersXYZ
 end
 
 end
